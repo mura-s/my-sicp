e8d30170f8e1f34493bc94d92903dd1ed3ff5604
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-03-09 23:01:29 +0900

    add 1.3.4 fixed-point-of-transform

--- ch1/ch1_3.scm
+++ ch1/ch1_3.scm
@@ -140,3 +140,19 @@
                     1.0))
 
 ;; (print (sqrt-newton 2))
+
+(define (fixed-point-of-transform g transform guess)
+    (fixed-point (transform g) guess))
+
+(define (sqrt-1 x)
+    (fixed-point-of-transform (lambda (y) (/ x y))
+                              average-damp
+                              1.0))
+
+(define (sqrt-2 x)
+    (fixed-point-of-transform (lambda (y) (- (square y) x))
+                              newton-transform
+                              1.0))
+
+;; (print (sqrt-1 2))
+;; (print (sqrt-2 2))
