5b31d4fb313cbc46675b13025f847fdd08d5708d
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-03-21 21:45:22 +0900

    add 2.2.2 tree map

--- ch2/ch2_2.scm
+++ ch2/ch2_2.scm
@@ -65,3 +65,21 @@
 (define x (cons (list 1 2) (list 3 4)))
 
 ; (print (count-leaves (list x x)))
+
+(define (scale-tree tree factor)
+    (cond ((null? tree) nil)
+          ((not (pair? tree)) (* tree factor))
+          (else (cons (scale-tree (car tree) factor)
+                      (scale-tree (cdr tree) factor)))))
+
+; (define t1 (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+; (print (scale-tree t1 2))
+
+(define (scale-tree2 tree factor)
+    (map (lambda (sub-tree)
+            (if (pair? sub-tree)
+                (scale-tree2 sub-tree factor)
+                (* sub-tree factor)))
+         tree))
+
+; (print (scale-tree2 t1 2))
