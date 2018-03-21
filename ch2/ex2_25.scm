21e79e81095e57ef8a8ee2710d0c6047aad1fdf3
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-03-21 13:48:18 +0900

    add ex2_25

---
+++ ch2/ex2_25.scm
@@ -0,0 +1,7 @@
+(define a (list 1 3 (list 5 7) 9))
+(define b (list (list 7)))
+(define c (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+
+(print (car (cdr (car (cdr (cdr a))))))
+(print (car (car b)))
+(print (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr c)))))))))))))
