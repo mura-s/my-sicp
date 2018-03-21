ec0abe44470cab765e230c86f26ea41d4f4a22f4
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-03-21 13:16:58 +0900

    add 2.2.2

--- ch2/ch2_2.scm
+++ ch2/ch2_2.scm
@@ -54,3 +54,14 @@
          items))
 
 ; (print (scale-list (list 1 2 3 4) 10))
+
+;; 2.2.2
+(define (count-leaves x)
+    (cond ((null? x) 0)
+          ((not (pair? x)) 1)
+          (else (+ (count-leaves (car x))
+                   (count-leaves (cdr x))))))
+
+(define x (cons (list 1 2) (list 3 4)))
+
+; (print (count-leaves (list x x)))
