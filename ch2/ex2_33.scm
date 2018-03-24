b910715c81f61d588a137e233d4d606ecbf8ded6
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-03-24 15:58:53 +0900

    add ex2_33

---
+++ ch2/ex2_33.scm
@@ -0,0 +1,30 @@
+(use compat.sicp)
+
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence) (accumulate op initial (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence) (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+    (accumulate (lambda (x y) (cons (p x) y))
+                nil
+                sequence))
+
+(print (map square (list 1 2 3 4 5)))
+
+(define (append seq1 seq2)
+    (accumulate cons seq2 seq1))
+
+(print (append (list 1 2 3) (list 4 5 6)))
+
+(define (length sequence)
+    (accumulate (lambda (x y) (+ 1 y))
+                0
+                sequence))
+
+(print (length (list 1 2 3 4 5)))
