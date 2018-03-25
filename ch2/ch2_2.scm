e7da60cdf5c696ad7e6a0ebe0f4f890d01de776c
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-03-25 15:11:38 +0900

    add 2.2.3 nested mappings

--- ch2/ch2_2.scm
+++ ch2/ch2_2.scm
@@ -178,3 +178,50 @@
                      (filter odd? sequence))))
 
 ; (print (product-of-squares-of-odd-elements (list 1 2 3 4 5)))
+
+(define (flatmap proc seq)
+    (accumulate append nil (map proc seq)))
+
+(define (smallest-divisor n)
+    (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+    (= (remainder b a) 0))
+
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+(define (prime-sum? pair)
+    (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+    (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+    (map make-pair-sum
+        (filter prime-sum?
+            (flatmap (lambda (i)
+                        (map (lambda (j) (list i j))
+                             (enumerate-interval 1 (- i 1))))
+                     (enumerate-interval 1 n)))))
+
+; (print (prime-sum-pairs 6))
+
+(define (remove item sequence)
+    (filter (lambda (x) (not (= x item)))
+            sequence))
+
+(define (permutations s)
+    (if (null? s)
+        (list nil)
+        (flatmap (lambda (x)
+                    (map (lambda (p) (cons x p))
+                         (permutations (remove x s))))
+                 s)))
+
+; (print (permutations (list 1 2 3)))
