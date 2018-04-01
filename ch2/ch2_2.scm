f98298c30dca135d49dcf341c1884a9d29f2e677
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-04-01 13:45:11 +0900

    add 2.2.4 Frames

--- ch2/ch2_2.scm
+++ ch2/ch2_2.scm
@@ -271,3 +271,12 @@
     (let ((combine4 (square-of-four flip-horiz identity
                                     rotate180 flip-vert)))
         (combine4 (corner-split painter n))))
+
+(define (frame-coord-map frame)
+    (lambda (v)
+        (add-vect
+            (origin-frame frame)
+            (add-vect (scale-vect (xcor-vect v)
+                                  (edge1-frame frame))
+                      (scale-vect (ycor-vect v)
+                                  (edge2-frame frame))))))
