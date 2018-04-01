1c41d2b3b0ce215de4f5d0f46f046ffa0bad1c93
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-04-01 14:11:15 +0900

    add 2.2.4 Painters

--- ch2/ch2_2.scm
+++ ch2/ch2_2.scm
@@ -280,3 +280,12 @@
                                   (edge1-frame frame))
                       (scale-vect (ycor-vect v)
                                   (edge2-frame frame))))))
+
+(define (segments->painter segment-list)
+    (lambda (frame)
+        (for-each
+            (lambda (segment)
+                (draw-line
+                    ((frame-coord-map frame) (start-segment segment))
+                    ((frame-coord-map frame) (end-segment segment))))
+            segment-list)))
