11296cf8444ff1914fe338ba4333b81372b5744a
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-04-08 18:21:35 +0900

    add 2.3.3

--- ch2/ch2_3.scm
+++ ch2/ch2_3.scm
@@ -67,3 +67,23 @@
 (print (deriv '(+ x 3) 'x))
 (print (deriv '(* x y) 'x))
 (print (deriv '(* (* x y) (+ x 3)) 'x))
+
+;; 2.3.3
+(define (element-of-set? x set)
+    (cond ((null? set) false)
+          ((equal? x (car set)) true)
+          (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+    (if (element-of-set? x set)
+        set
+        (cons x set)))
+
+(define (intersection-set set1 set2)
+    (cond ((or (null? set1) (null? set2))
+            '())
+          ((element-of-set? (car set1) set2)        
+            (cons (car set1)
+                  (intersection-set (cdr set1) set2)))
+          (else
+            (intersection-set (cdr set1) set2))))
