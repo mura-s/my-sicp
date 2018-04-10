18d518a1d39c312f99b08314f464311fc9b10c3e
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-04-10 22:48:35 +0900

    add ex2_66

---
+++ ch2/ex2_66.scm
@@ -0,0 +1,16 @@
+(use compat.sicp)
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (lookup given-key set-of-records)
+    (cond ((null? set-of-records) false)
+          ((= given-key (key (entry set-of-records)))
+            (entry set-of-records))
+          ((< given-key (key (entry set-of-records)))
+            (lookup given-key (left-branch set-of-records)))
+          ((> given-key (key (entry set-of-records)))
+            (lookup given-key (right-branch set-of-records)))))
