3d082ac4928971d7868e3edb8ac56a32cf68af06
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-01 14:33:03 +0900

    add ex2_89

---
+++ ch2/ex2_89.scm
@@ -0,0 +1,11 @@
+(use compat.sicp)
+
+(define (first-term term-list)
+    (make-term (- (length term-list) 1) (car term-list)))
+
+(define (adjoin-term term term-list)
+    (cond ((=zero? (coeff term)) term-list)
+          ((equ? (order term) (length term-list))
+            (cons (coeff term) term-list))
+          (else
+            (adjoin-term term (cons 0 term-list)))))
