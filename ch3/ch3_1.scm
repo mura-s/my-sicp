57891da541c3703d0a4ab175592c1ce69585bb2d
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-02 21:10:12 +0900

    add 3.1.3

--- ch3/ch3_1.scm
+++ ch3/ch3_1.scm
@@ -96,3 +96,34 @@
     (iter trials 0 initial-x))
 
 ; (print (estimate-pi-2 10000))
+
+; 3.1.3
+(define (make-simplified-withdraw balance)
+    (lambda (amount)
+        (set! balance (- balance amount))
+        balance))
+
+(define (make-decrementer balance)
+    (lambda (amount)
+        (- balance amount)))
+
+(define (factorial n)
+    (define (iter product counter)
+        (if (> counter n)
+            product
+            (iter (* counter product)
+                (+ counter 1))))
+    (iter 1 1))
+
+(define (factorial-set n)
+    (let ((product 1)
+          (counter 1))
+        (define (iter)
+        (if (> counter n)
+            product
+            (begin (set! product (* counter product))
+                   (set! counter (+ counter 1))
+                   (iter))))
+        (iter)))
+
+; (print (factorial-set 5))
