d6837025b492c8642c38ba48d5863dc4c3df7fe6
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-03 15:39:37 +0900

    add 3.2.4

--- ch3/ch3_2.scm
+++ ch3/ch3_2.scm
@@ -11,3 +11,15 @@
     (sum-of-squares (+ a 1) (* a 2)))
 
 (print (f 5))
+
+; 3.2.4
+(define (sqrt x)
+    (define (good-enough? guess)
+        (< (abs (- (square guess) x)) 0.001))
+    (define (improve guess)
+        (average guess (/ x guess)))
+    (define (sqrt-iter guess)
+        (if (good-enough? guess)
+            guess
+            (sqrt-iter (improve guess))))
+    (sqrt-iter 1.0))
