5b2e031f60d1522d4935e851d71448e5b8711143
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-03 14:43:23 +0900

    add 3.2.2

---
+++ ch3/ch3_2.scm
@@ -0,0 +1,13 @@
+(use compat.sicp)
+
+; 3.2.2
+(define (square x)
+    (* x x))
+
+(define (sum-of-squares x y)
+    (+ (square x) (square y)))
+
+(define (f a)
+    (sum-of-squares (+ a 1) (* a 2)))
+
+(print (f 5))
