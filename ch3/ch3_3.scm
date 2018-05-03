4aabc5f80e8d59b5e438ad25a5d600b23f77dbff
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-03 23:41:41 +0900

    add 3.3.1

--- ch3/ch3_3.scm
+++ ch3/ch3_3.scm
@@ -1,1 +1,24 @@
 (use compat.sicp)
+
+; 3.3.1
+(define x (list (cons 'a 'b) 'c 'd))
+(define y (list 'e 'f))
+
+(print x)
+(print y)
+
+(set-car! x y)
+(print x)
+
+(set-cdr! x y)
+(print x)
+
+(define (get-new-pair) (cons nil nil))
+
+(define (cons x y)
+  (let ((new (get-new-pair)))
+    (set-car! new x)
+    (set-cdr! new y)
+    new))
+
+(print (cons 'a 'b))
