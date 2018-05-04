9f027a163fac00e39a96d26d318fcf6618161967
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-05 00:27:12 +0900

    add 3.3.1 Sharing and identity

--- ch3/ch3_3.scm
+++ ch3/ch3_3.scm
@@ -25,3 +25,14 @@
 (newline)
 
 ; Sharing and identity
+(define x (list 'a 'b))
+(define z1 (cons x x))
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+
+(define (set-to-wow! x)
+    (set-car! (car x) 'wow)
+    x)
+
+(print (set-to-wow! z1))
+(print (set-to-wow! z2))
+(newline)
