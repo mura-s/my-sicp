7e2e18eef35eb60b5b9b73999ace54c29c61f46b
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-12 15:39:17 +0900

    add 3.3.4 The agenda

--- ch3/ch3_3_4.scm
+++ ch3/ch3_3_4.scm
@@ -88,3 +88,17 @@
 
 (define (add-action! wire action-procedure)
     ((wire 'add-action!) action-procedure))
+
+; The agenda
+(define (after-delay delay action)
+    (add-to-agenda! (+ delay (current-time the-agenda))
+                    action
+                    the-agenda))
+
+(define (propagate)
+    (if (empty-agenda? the-agenda)
+        'done
+        (let ((first-item (first-agenda-item the-agenda)))
+            (first-item)
+            (remove-first-agenda-item! the-agenda)
+            (propagate))))
