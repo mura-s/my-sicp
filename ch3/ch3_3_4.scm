811d0da4a5043adc59cc7394b28b7ab6f38c7cbc
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-12 16:30:08 +0900

    add 3.3.4 A sample simulation

--- ch3/ch3_3_4.scm
+++ ch3/ch3_3_4.scm
@@ -102,3 +102,34 @@
             (first-item)
             (remove-first-agenda-item! the-agenda)
             (propagate))))
+
+; A sample simulation
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()        
+                (newline)
+                (display name)
+                (display " ")
+                (display (current-time the-agenda))
+                (display "  New-value = ")
+                (display (get-signal wire)))))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+(probe 'sum sum)
+(probe 'carry carry)
+
+(half-adder input-1 input-2 sum carry)
+(set-signal! input-1 1)
+(propagate)
+
+(set-signal! input-2 1)
+(propagate)
