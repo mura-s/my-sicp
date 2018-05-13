d55137819bd75b65010bcefc50f44b8fef9ac9d7
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-13 22:47:22 +0900

    add 3.4.1

--- ch3/ch3_4.scm
+++ ch3/ch3_4.scm
@@ -1,3 +1,9 @@
 (use compat.sicp)
 
 ; 3.4.1
+
+;(define (withdraw amount)
+;    (if (>= balance amount)
+;        (begin (set! balance (- balance amount))
+;                balance)
+;        "Insufficient funds"))
