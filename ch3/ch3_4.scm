1577892e364b927840db48112dcd2dea89f8b824
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-14 23:02:25 +0900

    add 3.4.2 Serializers in Scheme

--- ch3/ch3_4.scm
+++ ch3/ch3_4.scm
@@ -7,3 +7,22 @@
 ;        (begin (set! balance (- balance amount))
 ;                balance)
 ;        "Insufficient funds"))
+
+; 3.4.2
+; Serializers in Scheme
+(define (make-account balance)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount))
+                balance)
+            "Insufficient funds"))
+    (define (deposit amount)
+        (set! balance (+ balance amount))
+        balance)
+    (let ((protected (make-serializer)))
+        (define (dispatch m)
+        (cond ((eq? m 'withdraw) (protected withdraw))
+              ((eq? m 'deposit) (protected deposit))
+              ((eq? m 'balance) balance)
+              (else (error "Unknown request -- MAKE-ACCOUNT" m))))
+        dispatch))
