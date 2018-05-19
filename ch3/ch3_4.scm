cd78d4f042571a0a63580b1dd2bb3fbd5eecc720
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-05-19 17:07:59 +0900

    add 3.4.2 Implementing serializers

--- ch3/ch3_4.scm
+++ ch3/ch3_4.scm
@@ -63,3 +63,32 @@
         ((serializer1 (serializer2 exchange))
             account1
             account2)))
+
+; Implementing serializers
+(define (make-serializer)
+    (let ((mutex (make-mutex)))
+        (lambda (p)
+            (define (serialized-p . args)
+                (mutex 'acquire)
+                (let ((val (apply p args)))
+                    (mutex 'release)
+                    val))
+            serialized-p)))
+
+(define (make-mutex)
+    (let ((cell (list false)))            
+        (define (the-mutex m)
+            (cond ((eq? m 'acquire)
+                    (if (test-and-set! cell)
+                        (the-mutex 'acquire))) ; retry
+                  ((eq? m 'release) (clear! cell))))
+        the-mutex))
+
+(define (clear! cell)
+    (set-car! cell false))
+
+(define (test-and-set! cell)
+    (if (car cell)
+        true
+        (begin (set-car! cell true)
+               false)))
