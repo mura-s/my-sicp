46a6e798875566bf15f70256b6539f6deec8cf07
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-06-03 14:21:23 +0900

    add ex3_68

---
+++ ch3/ex3_68.scm
@@ -0,0 +1,101 @@
+(use compat.sicp)
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define the-empty-stream '())
+(define stream-null? null?)
+
+(define (stream-ref s n)
+    (if (= n 0)
+        (stream-car s)
+        (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-filter pred stream)
+    (cond ((stream-null? stream)
+            the-empty-stream)
+          ((pred (stream-car stream))
+            (cons-stream (stream-car stream)
+                         (stream-filter pred (stream-cdr stream))))
+          (else
+             (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-map proc . argstreams)
+    (if (stream-null? (car argstreams))
+        the-empty-stream
+        (cons-stream
+            (apply proc (map stream-car argstreams))
+            (apply stream-map
+                   (cons proc (map stream-cdr argstreams))))))
+
+(define (add-streams s1 s2)
+    (stream-map + s1 s2))
+
+(define (partial-sums s)
+    (cons-stream (stream-car s)
+                 (add-streams (partial-sums s) (stream-cdr s))))
+
+(define (scale-stream stream factor)
+    (stream-map (lambda (x) (* x factor))
+                stream))
+
+(define (stream-for-each proc s)
+    (if (stream-null? s)
+        'done
+        (begin (proc (stream-car s))
+               (stream-for-each proc (stream-cdr s)))))
+
+(define (display-line x)
+    (newline)
+    (display x))
+
+(define (display-stream s)
+    (stream-for-each display-line s))
+
+(define (stream-head s n)
+    (define (iter s n)
+        (if (<= n 0)
+            'done
+            (begin
+                (display (stream-car s))
+                (newline)
+                (iter (stream-cdr s) (- n 1)))))
+    (iter s n))
+
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (integers-starting-from n)
+    (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (smallest-divisor n)
+    (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+    (= (remainder b a) 0))
+
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+(define (interleave s1 s2)
+    (if (stream-null? s1)
+        s2
+        (cons-stream (stream-car s1)
+                     (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+    (interleave
+        (stream-map (lambda (x) (list (stream-car s) x))
+                    t)
+        (pairs (stream-cdr s) (stream-cdr t))))
+
+(stream-head (pairs integers integers) 10)
+
+; (pairs (stream-cdr s) (stream-cdr t)) が遅延評価されないため、無限ループになってしまう。
