75d86f5152aa94c36e9ff7feb0ecc7e0976e7564
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-06-11 22:38:21 +0900

    add 3.5.5 Modularity of Functional Programs and Modularity of Objects

--- ch3/ch3_5_5.scm
+++ ch3/ch3_5_5.scm
@@ -63,3 +63,51 @@
     (iter s n))
 
 ; 3.5.5
+(define (pow y n)
+    (if (= n 0)
+        1
+        (* y (pow y (- n 1)))))
+
+(define random-init 1)
+
+(define (rand-update x)
+    (remainder (* 48271 x) (- (pow 2 31) 1)))
+
+(define rand
+    (let ((x random-init))
+        (lambda ()
+            (set! x (rand-update x))
+            x)))
+
+(define random-numbers
+    (cons-stream random-init
+                 (stream-map rand-update random-numbers)))
+
+; (stream-head random-numbers 10)
+
+(define (map-successive-pairs f s)
+    (cons-stream
+        (f (stream-car s) (stream-car (stream-cdr s)))
+        (map-successive-pairs f (stream-cdr (stream-cdr s)))))
+
+(define cesaro-stream
+    (map-successive-pairs (lambda (r1 r2) (= (gcd r1 r2) 1))
+                          random-numbers))
+
+; (stream-head cesaro-stream 10)
+
+(define (monte-carlo experiment-stream passed failed)
+    (define (next passed failed)
+        (cons-stream
+            (/ passed (+ passed failed))
+            (monte-carlo
+                (stream-cdr experiment-stream) passed failed)))
+    (if (stream-car experiment-stream)
+        (next (+ passed 1) failed)
+        (next passed (+ failed 1))))
+
+(define pi
+  (stream-map (lambda (p) (sqrt (/ 6 p)))
+              (monte-carlo cesaro-stream 0 0)))
+
+; (stream-head pi 1000)
