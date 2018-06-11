f033f4aa66254ead5fee25af61ea6e4774ab66af
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-06-11 16:06:38 +0900

    add 3.5.4 Streams and Delayed Evaluation

--- ch3/ch3_5_4.scm
+++ ch3/ch3_5_4.scm
@@ -63,3 +63,18 @@
     (iter s n))
 
 ; 3.5.4
+(define (integral delayed-integrand initial-value dt)
+    (define int
+        (cons-stream initial-value
+                     (let ((integrand (force delayed-integrand)))
+                        (add-streams (scale-stream integrand dt)
+                                     int))))
+    int)
+
+(define (solve f y0 dt)
+    (define y (integral (delay dy) y0 dt))
+    (define dy (stream-map f y))
+    y)
+
+; (print (stream-ref (solve (lambda (y) y) 1 0.001)
+;                    1000))
