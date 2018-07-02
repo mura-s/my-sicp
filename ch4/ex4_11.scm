556e5d93731b3cdd0e597615026b37f6a8df2b37
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-07-02 17:16:52 +0900

    add ex4_11

---
+++ ch4/ex4_11.scm
@@ -0,0 +1,22 @@
+(use compat.sicp)
+
+(define (make-frame variables values)
+    (if (null? variables)
+        '()
+        (cons (cons (car variables) (car values))
+              (make-frame (cdr variables) (cdr values)))))
+
+(define (frame-variables frame)
+    (if (null? frame)
+        '()
+        (cons (caar frame)
+              (frame-variables (cdr frame)))))
+
+(define (frame-values frame)
+    (if (null? frame)
+        '()
+        (cons (cdar frame)
+              (frame-variables (cdr frame)))))
+
+(define (add-binding-to-frame! var val frame)
+    (set! frame (cons (cons var val) frame)))
