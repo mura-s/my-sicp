21ae55ca00a1e37f936c7391110888fc0237da0e
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-08-10 15:24:00 +0900

    add ex4_22

---
+++ ch4/ex4_22.scm
@@ -0,0 +1,23 @@
+(use compat.sicp)
+
+(define (analyze exp)
+    (cond ((self-evaluating? exp) (analyze-self-evaluating exp))
+          ; 略
+          ((let? exp) (analyze (let-combination exp)))
+          ; 略
+          (else (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (let? exp) (tagged-list? exp 'let))
+(define (let-clauses exp) (cdr exp))
+(define (let-bindings clauses) (car clauses))
+(define (let-body clauses) (cdr clauses))
+
+(define (let-vars bindings) (map car bindings))
+(define (let-exps bindings) (map cadr bindings))
+
+(define (let->combination exp) (expand-let-clauses (let-clauses exp)))
+
+(define (expand-let-clauses clauses)
+    (cons (make-lambda (let-vars (let-bindings clauses))
+                       (let-body clauses))
+          (let-exps (let-bindings clauses))))
