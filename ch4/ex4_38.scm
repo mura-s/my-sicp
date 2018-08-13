7a85d5802390047af4e61b01661597d5f9673a29
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-08-13 18:25:13 +0900

    add ex4_38

---
+++ ch4/ex4_38.scm
@@ -0,0 +1,7 @@
+; 以下の、5通り
+;
+; Baker, Cooper, Miller, Fletcher, Smith
+; Baker, Cooper, Smith, Fletcher, Miller
+; Smith, Cooper, Baker, Fletcher, Miller
+; Baker, Fletcher, Smith, Cooper, Miller
+; Smith, Fletcher, Baker, Cooper, Miller
