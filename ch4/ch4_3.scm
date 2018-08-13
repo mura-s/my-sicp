ea61f7852f965e083fbbf4a4e7c2decb1c1f3b99
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-08-13 14:52:41 +0900

    add 4.3.1

---
+++ ch4/ch4_3.scm
@@ -0,0 +1,32 @@
+(use compat.sicp)
+
+(define (smallest-divisor n)
+    (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+    (= (remainder b a) 0))
+
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+; 4.3.1
+(define (require p)
+    (if (not p) (amb)))
+
+(define (an-element-of items)
+    (require (not (null? items)))
+    (amb (car items) (an-element-of (cdr items))))
+
+(define (an-integer-starting-from n)
+    (amb n (an-integer-starting-from (+ n 1))))
+
+(define (prime-sum-pair list1 list2)
+    (let ((a (an-element-of list1))
+          (b (an-element-of list2)))
+        (require (prime? (+ a b)))
+        (list a b)))
