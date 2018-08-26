c139614117bc595d55282470cba1f52bbb80d9d4
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-08-26 11:40:09 +0900

    Added ex4_45

---
+++ ch4/ex4_45.scm
@@ -0,0 +1,36 @@
+; The professor lectures to the student in the class with the cat
+
+; 1.
+; The professor
+;   lectures
+;     to the student
+;   in the class
+;   with the cat
+
+; 2.
+; The professor
+;   lectures
+;     to the student
+;       in the class
+;   with the cat
+
+; 3.
+; The professor
+;   lectures
+;     to the student
+;   in the class
+;     with the cat
+
+; 4.
+; The professor
+;   lectures
+;     to the student
+;       in the class
+;       with the cat
+
+; 5.
+; The professor
+;   lectures
+;     to the student
+;       in the class
+;         with the cat
