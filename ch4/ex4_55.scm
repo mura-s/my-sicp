4d506e89253e61c5279c935f132dbf418f8f9c54
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-09-09 15:24:37 +0900

    Added ex4_55

---
+++ ch4/ex4_55.scm
@@ -0,0 +1,8 @@
+; a. Ben Bitdiddleに監督されている人すべて; 
+(supervisor ?x (Bitdiddle Ben)) 
+
+; b. 経理部門[accounting division]のすべての人の名前と担当; 
+(job ?x (accounting . ?y)) 
+
+; c. Slumervilleに住む人すべての名前と住所. 
+(address ?x (Slumerville . ?y))
