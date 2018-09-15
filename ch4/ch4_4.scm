2a8d4dd1c143c96b152c572c4d3dc9112bea0682
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-09-15 15:16:02 +0900

    Added 4.4.2

--- ch4/ch4_4.scm
+++ ch4/ch4_4.scm
@@ -1,3 +1,10 @@
 (use compat.sicp)
 
 ; 4.4.1
+
+; 4.4.2
+; (assert! (job (Bitdiddle Ben) (computer wizard)))
+
+; (assert! (rule (wheel ?person)
+;             (and (supervisor ?middle-manager ?person)
+;                  (supervisor ?x ?middle-manager))))
