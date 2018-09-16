30a458d4f9b30e4c3481768e67a3b23ed9627e66
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-09-16 18:12:02 +0900

    Added ch4.4.4.2

--- ch4/ch4_4.scm
+++ ch4/ch4_4.scm
@@ -56,6 +56,15 @@
         (cons-stream (proc (stream-car s))
                      (stream-map proc (stream-cdr s)))))
 
+(define (stream-filter pred stream)
+    (cond ((stream-null? stream)
+            the-empty-stream)
+          ((pred (stream-car stream))
+            (cons-stream (stream-car stream)
+                         (stream-filter pred (stream-cdr stream))))
+          (else
+             (stream-filter pred (stream-cdr stream)))))
+
 (define (prompt-for-input string)
     (newline) (newline) (display string) (newline))
 
@@ -94,3 +103,66 @@
                 (cons (copy (car exp)) (copy (cdr exp))))
               (else exp)))
     (copy exp))
+
+; 4.4.4.2
+(define (qeval query frame-stream)
+    (let ((qproc (get (type query) 'qeval)))
+        (if qproc
+            (qproc (contents query) frame-stream)
+            (simple-query query frame-stream))))
+
+(define (simple-query query-pattern frame-stream)
+    (stream-flatmap
+        (lambda (frame)
+            (stream-append-delayed
+                (find-assertions query-pattern frame)
+                (delay (apply-rules query-pattern frame))))
+        frame-stream))
+
+(define (conjoin conjuncts frame-stream)
+    (if (empty-conjunction? conjuncts)
+        frame-stream
+        (conjoin (rest-conjuncts conjuncts)
+                 (qeval (first-conjunct conjuncts)
+                        frame-stream))))
+(put 'and 'qeval conjoin)
+
+(define (disjoin disjuncts frame-stream)
+    (if (empty-disjunction? disjuncts)
+        the-empty-stream
+        (interleave-delayed
+            (qeval (first-disjunct disjuncts) frame-stream)
+            (delay (disjoin (rest-disjuncts disjuncts)
+                            frame-stream)))))
+(put 'or 'qeval disjoin)
+
+(define (negate operands frame-stream)
+    (stream-flatmap
+        (lambda (frame)
+            (if (stream-null? (qeval (negated-query operands)
+                                     (singleton-stream frame)))
+                (singleton-stream frame)
+                the-empty-stream))
+        frame-stream))
+(put 'not 'qeval negate)
+
+(define (lisp-value call frame-stream)
+    (stream-flatmap
+        (lambda (frame)
+            (if (execute
+                    (instantiate
+                        call
+                        frame
+                        (lambda (v f)
+                            (error "Unknown pat var -- LISP-VALUE" v))))
+                (singleton-stream frame)
+                the-empty-stream))
+        frame-stream))
+(put 'lisp-value 'qeval lisp-value)
+
+(define (execute exp)
+    (apply (eval (predicate exp) user-initial-environment)
+           (args exp)))
+
+(define (always-true ignore frame-stream) frame-stream)
+(put 'always-true 'qeval always-true)
