327ebf99a3fbcff34497617b2f84917a55de7745
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-09-22 13:57:46 +0900

    Add 5.1.1

--- ch5/ch5_1.scm
+++ ch5/ch5_1.scm
@@ -1,5 +1,33 @@
+(use compat.sicp)
+
 ; 5.1
 (define (gcd a b)
     (if (= b 0)
         a
         (gcd b (remainder a b))))
+
+; 5.1.1
+; (controller
+;     test-b
+;         (test (op =) (reg b) (const 0))
+;         (branch (label gcd-done))
+;         (assign t (op rem) (reg a) (reg b))
+;         (assign a (reg b))
+;         (assign b (reg t))
+;         (goto (label test-b))
+;     gcd-done)
+
+; (controller
+;     gcd-loop
+;         (assign a (op read))
+;         (assign b (op read))
+;     test-b
+;         (test (op =) (reg b) (const 0))
+;         (branch (label gcd-done))
+;         (assign t (op rem) (reg a) (reg b))
+;         (assign a (reg b))
+;         (assign b (reg t))
+;         (goto (label test-b))
+;     gcd-done
+;         (perform (op print) (reg a))
+;         (goto (label gcd-loop)))
