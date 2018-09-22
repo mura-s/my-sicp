dad58058513839936de4ddec44102509e34e7932
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-09-22 14:07:41 +0900

    Add 5.1.2

--- ch5/ch5_1.scm
+++ ch5/ch5_1.scm
@@ -31,3 +31,25 @@
 ;     gcd-done
 ;         (perform (op print) (reg a))
 ;         (goto (label gcd-loop)))
+
+; 5.1.2
+(define (remainder n d)
+    (if (< n d)
+        n
+        (remainder (- n d) d)))
+
+; (controller
+;     test-b
+;         (test (op =) (reg b) (const 0))
+;         (branch (label gcd-done))
+;         (assign t (reg a))
+;     rem-loop
+;         (test (op <) (reg t) (reg b))
+;         (branch (label rem-done))
+;         (assign t (op -) (reg t) (reg b))
+;         (goto (label rem-loop))
+;     rem-done
+;         (assign a (reg b))
+;         (assign b (reg t))
+;         (goto (label test-b))
+;     gcd-done)
