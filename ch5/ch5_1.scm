a2d8b7df5f07c370e2e3f5890f96406ec05105fb
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-09-22 16:35:29 +0900

    Add 5.1.3

--- ch5/ch5_1.scm
+++ ch5/ch5_1.scm
@@ -53,3 +53,25 @@
 ;         (assign b (reg t))
 ;         (goto (label test-b))
 ;     gcd-done)
+
+; 5.1.3
+; gcd
+;     (test (op =) (reg b) (const 0))
+;     (branch (label gcd-done))
+;     (assign t (op rem) (reg a) (reg b))
+;     (assign a (reg b))
+;     (assign b (reg t))
+;     (goto (label gcd))
+; gcd-done
+;     (goto (reg continue))
+  
+;     ;; gcdを呼び出す前に, gcdから戻るべきラベルを
+;     ;; continueに代入する.
+;     (assign continue (label after-gcd-1))
+;     (goto (label gcd))
+; after-gcd-1
+  
+;     ;; gcdへの第二の呼出しでは継続は異る.
+;     (assign continue (label after-gcd-2))
+;     (goto (label gcd))
+; after-gcd-2
