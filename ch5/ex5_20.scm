dcfc6a9092c95058463aa92b436398056d4d9f2e
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-10-21 16:20:32 +0900

    Add ex5_20

---
+++ ch5/ex5_20.scm
@@ -0,0 +1,8 @@
+index    | 0 | 1  | 2  | 3  | 4
+---------------------------------
+the-cars |   | n1 | p1 | p1 |
+the-cdrs |   | n2 | p3 | e0 |
+
+; freeの最後の値: p4
+
+; ref. http://www.serendip.ws/archives/3384
